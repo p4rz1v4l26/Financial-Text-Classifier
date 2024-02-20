d506102909efe873a68a409b8bf4eed546d763ed
p4rz1v4l26 <20211cst0067@gmail.com>
2024-02-20 22:05:28 +0530

    .

--- index.py
+++ index.py
@@ -8,3 +8,11 @@ label_encoder=pickle.load(open(''))
 def process(inPath,outPath):
     input_df=pd.read_csv(inPath)
     features=tfidf_vectorizer.transform(input_df['body'])
+    
+    predictions = model.predict(features)
+  # convert output labels to categories
+    input_df['category'] = label_encoder.inverse_transform(predictions)
+  # save results to csv
+    output_df = input_df[['id', 'category']]
+    output_df.to_csv(outPath, index=False)
+
